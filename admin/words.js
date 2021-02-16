1eb24e1f20d025b350cface5bd7d3747661cc802
git-kick <github@kick-web.de>
2021-02-16 11:46:16 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"e3dc-rscp adapter settings": {
+		"en": "Adapter settings for e3dc-rscp",
+		"de": "Adaptereinstellungen für e3dc-rscp",
+		"ru": "Настройки адаптера для e3dc-rscp",
+		"pt": "Configurações do adaptador para e3dc-rscp",
+		"nl": "Adapterinstellingen voor e3dc-rscp",
+		"fr": "Paramètres d'adaptateur pour e3dc-rscp",
+		"it": "Impostazioni dell'adattatore per e3dc-rscp",
+		"es": "Ajustes del adaptador para e3dc-rscp",
+		"pl": "Ustawienia adaptera dla e3dc-rscp",
+		"zh-cn": "e3dc-rscp的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
