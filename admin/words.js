fdc49e5058983ff7b4032d0b7eb5006618f5d061
git-kick <github@kick-web.de>
2021-10-09 12:33:44 +0200

    first version used for switching wallbox

--- admin/words.js
+++ admin/words.js
@@ -15,7 +15,7 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla e3dc-rscp",
 		"zh-cn": "e3dc-rscp的适配器设置"
 	},
-	"e3dc-ip": {
+	"e3dc_ip": {
 		"en": "E3/DC IP address",
 		"de": "E3/DC IP-Adresse",
 		"ru": "Опция 1",
@@ -27,10 +27,10 @@ systemDictionary = {
 		"pl": "opcja 1",
 		"zh-cn": "选项1"
 	},
-	"e3dc-port": {
+	"e3dc_port": {
 		"en": "E3/DC port",
 		"de": "E3/DC Port",
-		"ru": "option2",
+		"ru": "option 2",
 		"pt": "opção 2",
 		"nl": "Optie 2",
 		"fr": "Option 2",
@@ -38,5 +38,53 @@ systemDictionary = {
 		"es": "opcion 2",
 		"pl": "Opcja 2",
 		"zh-cn": "选项2"
+	},
+	"rscp_password": {
+		"en": "E3/DC RSCP password",
+		"de": "E3/DC RSCP Passwort",
+		"ru": "option 3",
+		"pt": "opção 3",
+		"nl": "Optie 3",
+		"fr": "Option 3",
+		"it": "opzione 3",
+		"es": "opcion 3",
+		"pl": "Opcja 3",
+		"zh-cn": "选项3"
+	},
+	"portal_user": {
+		"en": "E3/DC portal user",
+		"de": "E3/DC Portal Benutzername",
+		"ru": "option 4",
+		"pt": "opção 4",
+		"nl": "Optie 4",
+		"fr": "Option 4",
+		"it": "opzione 4",
+		"es": "opcion 4",
+		"pl": "Opcja 4",
+		"zh-cn": "选项4"
+	},
+	"portal_password": {
+		"en": "E3/DC portal password",
+		"de": "E3/DC Portal Passwort",
+		"ru": "option 5",
+		"pt": "opção 5",
+		"nl": "Optie 5",
+		"fr": "Option 5",
+		"it": "opzione 5",
+		"es": "opcion 5",
+		"pl": "Opcja 5",
+		"zh-cn": "选项5"
+	},
+	"polling_interval": {
+		"en": "Polling interval in milliseconds",
+		"de": "Abfrageintervall in Millisekunden",
+		"ru": "option 6",
+		"pt": "opção 6",
+		"nl": "Optie 6",
+		"fr": "Option 6",
+		"it": "opzione 6",
+		"es": "opcion 6",
+		"pl": "Opcja 6",
+		"zh-cn": "选项6"
 	}
 };
