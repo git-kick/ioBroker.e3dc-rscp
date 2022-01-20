b8076adfced3cf2db3ca5c300732aa23c9f9a55c
git-kick <github@kick-web.de>
2022-01-20 08:02:49 +0100

    Removed Sentry plugin

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
 /* eslint-disable no-unused-vars */
 "use strict";
 
-const Sentry = require("@sentry/node");
-// or use es6 import statements
-// import * as Sentry from '@sentry/node';
-
-// const Tracing = require("@sentry/tracing");
-// or use es6 import statements
-// import * as Tracing from '@sentry/tracing';
-
-Sentry.init({
-	dsn: "https://cc70dc76ca0d4bc89be51866648d109c@o1065834.ingest.sentry.io/6058026",
-
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for performance monitoring.
-	// We recommend adjusting this value in production
-	tracesSampleRate: 1.0,
-});
-
-const transaction = Sentry.startTransaction({
-	op: "test",
-	name: "My First Test Transaction",
-});
-
 // System dictionary
 const fs = require("fs");
 // eslint-disable-next-line prefer-const
@@ -1786,15 +1764,6 @@ if (module.parent) {
 } else {
 	// otherwise start the instance directly
 	const ad = new E3dcRscp();
-	setTimeout(() => {
-		try {
-			ad.sendNextFrame();
-		} catch (e) {
-			Sentry.captureException(e);
-		} finally {
-			transaction.finish();
-		}
-	}, 99);
 }
 
 //
