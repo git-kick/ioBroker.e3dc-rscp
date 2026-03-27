24e8aef0da30091b6a1c575db8986eb10fe36e75
git-kick <iobroker@kick-web.de>
2026-03-27 16:17:20 +0100

    see https://github.com/prettier/eslint-plugin-prettier?tab=readme-ov-file#configuration-new-eslintconfigjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,3 +33,10 @@ export default [
         },
     },
 ];
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
