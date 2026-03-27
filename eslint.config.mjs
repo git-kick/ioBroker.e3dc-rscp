09b4d927ddfee33912dd7e74fa7a5c7198b9fb94
git-kick <iobroker@kick-web.de>
2026-03-27 16:46:27 +0100

    ESLint9 compatible Flat Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,34 @@
 // ioBroker eslint template configuration file for js and ts files
-// Please note that esm or react based modules need additional modules loaded.
-import config from '@iobroker/eslint-config';
+
+import config from "@iobroker/eslint-config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
     ...config,
+    eslintPluginPrettierRecommended,
     {
-        // specify files to exclude from linting here
         ignores: [
-            '.dev-server/',
-            '.vscode/',
-            '*.test.js',
-            'test/**/*.js',
-            '*.config.mjs',
-            'build',
-            'dist',
-            'admin/build', 
-            'admin/words.js',
-            'admin/admin.d.ts',
-            'admin/blockly.js',
-            '**/adapter-config.d.ts',
+            ".dev-server/",
+            ".vscode/",
+            "*.test.js",
+            "test/**/*.js",
+            "*.config.mjs",
+            "build",
+            "dist",
+            "admin/build",
+            "admin/words.js",
+            "admin/admin.d.ts",
+            "admin/blockly.js",
+            "**/adapter-config.d.ts",
         ],
     },
     {
-        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
-        // as this improves maintainability. jsdoc warnings will not block build process.
         rules: {
-            'jsdoc/require-jsdoc': 'warn',
-            'jsdoc/require-param': 'warn',
-            'jsdoc/require-param-description': 'warn',
-            'jsdoc/require-returns-description': 'warn',
-            'jsdoc/require-returns-check': 'warn',
+            "jsdoc/require-jsdoc": "warn",
+            "jsdoc/require-param": "warn",
+            "jsdoc/require-param-description": "warn",
+            "jsdoc/require-returns-description": "warn",
+            "jsdoc/require-returns-check": "warn",
         },
     },
-];
-
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-
-module.exports = [
-  // Any other config imports go at the top
-  eslintPluginPrettierRecommended,
 ];
